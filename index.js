609d0a34b706271b43f4b920ce55e170d08cb224
Abhishek Patel <ap8209655@gmail.com>
2021-07-06 12:02:39 +0530

    Connected to the databse MongoDB

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require("express");
+
 const port = 8000;
+const mongoose = require("./config/mongoose");
 const app = express();
-app.get("/", function (req, res) {
-  return res.send("Welcome to finance App");
-});
+
+app.use(express.static("./assets"));
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+app.use("/", require("./routes"));
 app.listen(port, function (err) {
   if (err) {
     console.log("Error in creating App", err);
